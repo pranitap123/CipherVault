b6883467b4bb3ebe8726f3faa0e66973ed89af5c
Pranita Panchal <pranitapanchal339@gmail.com>
2026-07-31 16:04:08 +0530

    feat(security): add security middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,17 @@ import authRouter from "./auth/authRouter.js";
 import fileRouter from "./files/filesRouter.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { logger } from "./middlewares/logger.js";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(compression());
 
 app.use(express.json());
 
